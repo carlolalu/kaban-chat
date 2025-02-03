cf9e8bed632e4227864de69c41d31b433885bfe9
Carlo Lanzi Luciani <carlo.lanzi.luciani@intra2net.com>
2025-02-03 09:22:28 +0100

    Add Message
    Message is the basic msg unit of exchange of the chat. The username
    and the message have a maximum length.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 // ########################################
-
-
-
-
-
+use serde::{Deserialize, Serialize};
 
 // ########################################
 pub mod constant {
@@ -11,5 +7,42 @@ pub mod constant {
     pub const MAX_NUM_USERS: usize = 2000;
 }
 
-
 // ########################################
+/// Message is the type of packet unit that the client uses. They comprehend a username of at most
+/// MAX_USERNAME_LEN chars and a content of at most MAX_CONTENT_LEN chars.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Message {
+    username: String,
+    content: String,
+}
+
+impl Message {
+    /// Maximal length (in chars) of the username.
+    pub const MAX_USERNAME_LEN: usize = 32;
+
+    /// Maximal length (in chars) of the content of the message.
+    pub const MAX_CONTENT_LEN: usize = 256;
+
+    pub fn new(username: &str, content: &str) -> Message {
+        if username.len() > Message::MAX_USERNAME_LEN {
+            panic!("username too long");
+        }
+
+        if content.len() > Message::MAX_CONTENT_LEN {
+            panic!("content too long");
+        }
+
+        Message {
+            username: username.to_string(),
+            content: content.to_string(),
+        }
+    }
+
+    pub fn get_username(&self) -> String {
+        self.username.clone()
+    }
+
+    pub fn get_content(&self) -> String {
+        self.content.clone()
+    }
+}
