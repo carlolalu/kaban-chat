132a48c7d867ea45d68e9c677f50f1e341c329b6
carlolalu <carlolanziluciani@gmail.com>
2025-02-08 12:11:09 +0100

    Add 'message_paketer' to lib and test it
    * Write new function handling the depaketization of messages
    through an AsyncWriter
    
    * test such function
    
    * test its communication with the function which reades messages
    
    Notice, since graceful shutdown has not been implemented yet, the
    test of 'message_depaketer' is not automatically returning without
    a CTRL+C. This must be addressed in the future to have a clean test
    result.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
 use serde_json::error::Category;
 use tokio::{
-    io::{AsyncRead, AsyncReadExt},
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
     sync,
 };
+
+use std::io::Write;
 
 use tokio_stream::StreamExt;
 
@@ -302,6 +304,122 @@ pub enum MsgDepaketerError {
     },
 }
 
+// ############################## ASYNC WRITE FUNCTION ##############################
+
+/// This function reads from the stdin with a 'prompt_buffer', stringify the result, packs it into
+/// many different messages with the given username and sends them through the tcp_writer.
+pub async fn message_paketer(
+    mut stdin: impl AsyncRead + Unpin,
+    mut tcp_writer: impl AsyncWrite + Unpin,
+    username: &str,
+    // cancellation_token: CancellationToken
+) -> Result<(), MsgPaketerError> {
+    let mut prompt_buffer: Vec<u8> = Vec::with_capacity(Message::MAX_CONTENT_LEN);
+    let mut previous_prompt_fragment: Vec<u8> = Vec::with_capacity(3);
+
+    'accepting_new_prompt: loop {
+        print!("{username}:> ");
+        std::io::stdout().flush()?;
+
+        previous_prompt_fragment.clear();
+
+        'processing_prompt_buffer: loop {
+            // Loop assumption: previous_prompt_fragment is initialised (eventually empty)
+
+            prompt_buffer.clear();
+
+            match stdin.read_buf(&mut prompt_buffer).await {
+                Ok(n) if n > 0 => {
+                    let is_last_chunk = if prompt_buffer.last() == Some(&b'\n') {
+                        prompt_buffer.pop();
+                        true
+                    } else {
+                        false
+                    };
+
+                    if prompt_buffer.is_empty() {
+                        continue 'accepting_new_prompt;
+                    }
+
+                    let chunk_to_process: Vec<u8> = previous_prompt_fragment
+                        .iter()
+                        .chain(prompt_buffer.iter())
+                        .map(|&byte| byte)
+                        .collect();
+
+                    let text = match String::from_utf8(chunk_to_process.clone()) {
+                        Ok(text) => {
+                            previous_prompt_fragment.clear();
+                            text
+                        }
+                        Err(error) => match error.utf8_error().error_len() {
+                            None => {
+                                let last_valid_index = error.utf8_error().valid_up_to();
+                                let text_result = String::from_utf8(
+                                    chunk_to_process[..last_valid_index].to_vec().clone(),
+                                );
+
+                                previous_prompt_fragment =
+                                    chunk_to_process[last_valid_index..].to_vec();
+
+                                text_result?
+                            }
+                            Some(_len) => return Err(MsgPaketerError::from(error)),
+                        },
+                    };
+
+                    let msg_results: Vec<Result<Message, TextValidityError>> = text
+                        .chars()
+                        .collect::<Vec<char>>()
+                        .chunks(Message::MAX_CONTENT_LEN)
+                        .map(|char_slice| String::from_iter(char_slice))
+                        .map(|content| Message::new(username, &content))
+                        .collect();
+
+                    let mut msg_result_stream = tokio_stream::iter(msg_results);
+
+                    while let Some(msg_result) = msg_result_stream.next().await {
+                        let paket = msg_result?.paket()?;
+                        tcp_writer.write_all(&paket).await?;
+                    }
+
+                    if is_last_chunk {
+                        continue 'accepting_new_prompt;
+                    } else {
+                        continue 'processing_prompt_buffer;
+                    }
+                }
+                // todo: handle the transmission of 0 bytes with graceful shutdown. It is an
+                // ambigous outcome: the tokio documentation claims this means an EOF and thus
+                // that the reader could still return something, while the empirics tells me that
+                // this happens only when the reader is unlinked from its source. I should verify.
+                Ok(_null) => continue 'accepting_new_prompt,
+                Err(error) => return Err(MsgPaketerError::from(error)),
+            }
+        }
+    }
+
+    // todo: remove this when adding graceful shutdown
+    #[allow(unreachable_code)]
+    Ok(())
+}
+
+#[derive(Error, Debug)]
+#[error(transparent)]
+pub enum MsgPaketerError {
+    Io(#[from] std::io::Error),
+    TxReceive(#[from] tokio::sync::mpsc::error::TryRecvError),
+    #[error("Unsual None returned from '{entity}' in 'handle_msgs_reader'.")]
+    UnusualEmptyEntity {
+        entity: String,
+    },
+    StringFromUtf8(#[from] std::string::FromUtf8Error),
+    TextValidity(#[from] TextValidityError),
+    SerdeJsonError(#[from] serde_json::Error),
+}
+
+// ############################## TEST UTILITIES ##############################
+
 pub mod test_util {
     use crate::*;
 
@@ -324,7 +442,9 @@ pub mod test_util {
 
 #[cfg(test)]
 pub mod test {
+    use crate::test_util::craft_random_valid_text;
     use crate::*;
+    use tokio::net::TcpStream;
     use tokio_util::task::TaskTracker;
 
     #[test]
@@ -488,5 +608,85 @@ pub mod test {
 
         task_tracker.close();
         task_tracker.wait().await;
+    }
+
+    #[tokio::test]
+    async fn test_message_paketer() {
+        let num_messages_prompt = 100;
+
+        let prompt_buffer0 = "Nam ignoratione rerum bonarum et malarum maxime
+hominum vita vexatur ob eumque errorem et voluptatibus
+maximis saepe privantur et durissimis animi doloribus
+torquentur.
+Ergo sapientia est adhibenda, quae et terroribus
+cupiditatibusque detractis et omnium falsarum opinionum
+temeritate derepta se nobis certissimam ducem praebeat ad
+voluptatem\n"
+            .to_string();
+
+        let prompt_buffer1 = format!(
+            "{}\n",
+            craft_random_valid_text(Message::MAX_CONTENT_LEN * num_messages_prompt)
+        );
+
+        let prompt_buffer2 = (1..=num_messages_prompt).map(|num| -> Vec<char> {
+            let prefix_len = "MESSAGE_n__[[[]]]\n".chars().count() + 1_usize + (num as f64).log10().floor() as usize;
+            let msg = format!{"MESSAGE_n_{}_[[[{}]]]\n", num, craft_random_valid_text(Message::MAX_CONTENT_LEN - prefix_len)};
+            msg.chars().collect()
+        }).flatten().collect::<String>();
+
+        let stdin = tokio_test::io::Builder::new()
+            .read(prompt_buffer0.as_bytes())
+            .read(prompt_buffer1.as_bytes())
+            .read(prompt_buffer2.as_bytes())
+            .build();
+
+        let stdin = tokio::io::BufReader::new(stdin);
+
+        let (tx_depaketer, mut rx_depaketer) =
+            tokio::sync::mpsc::channel::<Result<Message, MsgFromPaketError>>(20);
+
+        let test_task_tracker = TaskTracker::new();
+
+        // server
+        let server = test_task_tracker.spawn(async move {
+            let listener = tokio::net::TcpListener::bind(constant::SERVER_ADDR)
+                .await
+                .expect("point0");
+
+            let (socket_stream_server, _addr) = listener.accept().await.expect("point1");
+
+            message_depaketer(socket_stream_server, tx_depaketer)
+                .await
+                .unwrap();
+        });
+
+        // client
+        let client = test_task_tracker.spawn(async move {
+            let socket_stream_client = TcpStream::connect(constant::SERVER_ADDR)
+                .await
+                .expect("point2");
+
+            message_paketer(stdin, socket_stream_client, "peppino")
+                .await
+                .expect("I just exited the message paketer with an error");
+        });
+
+        while let Ok(msg) = rx_depaketer
+            .recv()
+            .await
+            .expect("something went wrong in the message transmission")
+        {
+            println!("{}:> {}", msg.get_username(), msg.get_content());
+        }
+
+        // todo: add graceful shutdown in the test as well: a clean "Ok" for the test is auspicable.
+
+        test_task_tracker.close();
+        test_task_tracker.wait().await;
+
+        if server.await.is_err() || client.await.is_err() {
+            panic!("One of the two handles panicked");
+        }
     }
 }
