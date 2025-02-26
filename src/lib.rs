acdf53c2bbee23aed7f7d5c701c64a4e948ffd97
Carlo Lanzi Luciani <carlo.lanzi.luciani@intra2net.com>
2025-02-26 10:57:18 +0100

    Rename module 'struct' to 'information_units' + remove unnecessary 'pub's

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-pub mod async_functions;
-pub mod structs;
-pub mod test_util;
+mod async_functions;
+mod information_units;
+mod test_util;
 
 pub mod prelude {
     pub use crate::async_functions::*;
-    pub use crate::structs::*;
+    pub use crate::information_units::*;
     pub use crate::test_util::*;
 }
